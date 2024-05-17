4fac8affc054f9fc5f2a4e16636d0a324e28a6e5
Torbjørn <diamond.up299@gmail.com>
2024-05-17 20:46:47 +0200

    potatoe

--- Main.js
+++ Main.js
@@ -1,3 +1,10 @@
+/*
+    some context:
+        this is pretty much just some utilities you can use for js
+        print,newEle,modifyStyle,clamp,GetMaxRGBbit
+    
+    enjoy, or don't, I don't care all that much lol
+*/
 const print = console.log;
 const newEle = (typ,par,props) => {
     par = par || document.body;
@@ -6,6 +13,35 @@ const newEle = (typ,par,props) => {
     for (let prop in props) {
         ele[prop] = props[prop];
     }
+    return ele;
 }
-
-newEle('h1', document.body, {innerText:'Hello, world!'});
+const modifyStyle = (ele, style) => {
+    for (let prop in style) {
+        ele.style[prop] = style[prop];
+    }
+}
+const clamp = (val,min,max) => {
+    if (val > max) return max;
+    if (val < min) return min;
+    return val;
+}
+const GetMaxRGBbit = (percentage) => {
+    return 255*(clamp(percentage,.0001,100)/100);
+}
+let ele1 = newEle('h1', document.body, {innerText:'sample text content epic'});
+let sine = 0;
+modifyStyle(document.body, {
+    backgroundColor: `rgb(255,${GetMaxRGBbit(50)},0)`,
+    color: `rgb(${GetMaxRGBbit(50)},${GetMaxRGBbit(25)},0)`
+})
+setInterval(() => {
+    sine++;
+    modifyStyle(ele1, {
+        left: 35+Math.sin(sine/175)*25 + '%',
+        top: 50+Math.sin((sine/175)-1.75)*25 + '%',
+        transform: `rotate(${Math.sin((sine/175)-1.5)*45}deg)`,
+        position: 'absolute',
+        fontFamily: 'monospace',
+        userSelect: 'none'
+    });
+}, 1);
